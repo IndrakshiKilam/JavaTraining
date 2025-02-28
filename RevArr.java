96f5f51aead818e6925e184d53c6e8a63b6f1446
IndrakshiKilam <indrakshikilam@gmail.com>
2025-02-28 21:46:22 +0530

    Initial commit

---
+++ RevArr.java
@@ -0,0 +1,55 @@
+public class RevArr{
+	
+	public static void main(String[] args) {
+		int[] a = {27,15,76,22,53,32,03,34,15,100,48};
+		int start = 0;
+		int end = a.length-1;
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		int temp = 0;
+		
+		System.out.println("Array before reversal\n");
+		int i = 0;
+		while(i<=end) {
+			System.out.println(a[i]+"\t");
+			i++;
+		}
+		
+		int x = start;
+		int y = end;
+		
+		while(start<=end) {
+			if(a[start]>max) {
+				max = a[start];
+			}
+			else if(a[start]<min) {
+				min = a[start];
+			}
+			if(a[end]>max) {
+				max = a[end];
+			}
+			else if(a[end]<min) {
+				min = a[end];
+			}
+			temp = a[start];
+			a[start] = a[end];
+			a[end] = temp;
+			
+			start++;
+			end--;
+		}
+		
+//		System.out.println(x+" "+y);
+		
+		System.out.println("Array after reversal\n");
+		int j = 0;
+		while(j<=y) {
+			System.out.println(a[j]+"\t");
+			j++;
+		}
+		
+		System.out.println("Max Value is : "+max);
+		System.out.println("Min Value is : "+min);
+	}
+}
